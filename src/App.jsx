5ee8aad4e9e117766c3f17096f831148adb58f8c
nenad2504 <nenad89obr@gmail.com>
2023-09-26 19:52:43 +0200

    cleaned app and added new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import Header from './components/Header.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { 
+  const name = "Nenad";
+
 
   return (
     <>
-      <h1>Task Tracker</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='container'>
+      <Header ime={name} />
+    </div>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+const Header = (props) => {
+  const onClick = () => {
+    console.log('Click');
+}
+
+  return (
+    <header className='header'>
+        <h1>Task Tracker</h1>
+        <h2>Hello, {props.ime}</h2>
+        <Button color='blue' text='Hello' onClick={onClick} />
+    </header>
+  )
+}
+
+Header.propTypes = {
+  ime: PropTypes.string.isRequired,
+};
+
+export default Header
